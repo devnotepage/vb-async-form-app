d6815c4d728ca3bb8c01dda38e4b7c7149983f5c
devnotepage <devnotepage@gmail.com>
2022-09-28 09:57:44 +0900

    update

--- git-sync.sh
+++ git-sync.sh
@@ -1,7 +1,13 @@
+git add .
+printf "\n"
+
+git commit -m'update'
+printf "\n"
+
 git pull -v --progress "origin"
 printf "\n"
 
-git push -v --progress "origin" master:master
+git push -v --progress "origin" main:main
 printf "\n"
 
 read -p "Complete!"
